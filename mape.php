acf7f13ed38ec8b339484b5ebc254d00196ecb0d
Fitra R <39044004+Rizki36@users.noreply.github.com>
2024-05-29 23:24:03 +0700

    feat: add mape

---
+++ mape.php
@@ -0,0 +1,49 @@
+<?php
+include "style.css";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Sistem peramalan persediaan obat</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- start css -->
+<link rel="stylesheet" href="penjualan.css">
+
+<!-- start font awesome -->
+<script src="https://kit.fontawesome.com/2c5a7f5b8c.js" crossorigin="anonymous"></script>
+<!-- end font awesome-->
+
+
+
+</head>
+<body>
+    <div class="sidenav">
+        <?php include 'sidebar.php'; ?>
+    </div>
+
+    <div style='flex: 1; max-height: 100vh; overflow: auto; padding-left: 36px; padding-right: 36px; padding-top: 36px'>
+        <h1 style="margin-bottom: 24px">PERAMALAN</h1>
+        
+        Nama obat<br>
+
+        <form action="mape.php" method="get">
+            <select id="nama_obat" name="nama_obat">
+                <option value="">Pilih obat</option>
+                <option value="Amoxicillin 500 MG HJ Isi 200">Amoxicillin 500 mg HJ isi 200</option>
+                <option value="Alpara Tablet">Alpara Tablet</option>
+                <option value="Antangin JRG Cair Dewasa">Antangin JRG Cair Dewasa</option>
+            </select>
+            <button>Pilih obat</button>
+        </form>
+        <br><br>
+
+        <?php 
+            if(isset($_GET['nama_obat']) && !empty($_GET['nama_obat'])){
+                include "mape_proses.php"; 
+            }
+        ?>
+    </div>
+</body>
+</html>
--- mape_proses.php
+++ mape_proses.php
@@ -17,7 +17,16 @@ $list = $result->fetch_all(MYSQLI_ASSOC);
             <th>Periode</th>
             <th>Jumlah Penjualan</th>
             <th>WMA</th>
+            <th></th>
+            <th>Mape</th>
         </tr>
+
+    <?php
+        $total_mape = 0;
+        $total_data = count($list);
+        $total_wma = 0;
+        $total_selisih = 0;
+    ?>
 
     <?php foreach($list as $index => $row) : ?>
         <?php 
@@ -30,26 +39,43 @@ $list = $result->fetch_all(MYSQLI_ASSOC);
                 $mape = (abs($row['jumlah_penjualan'] - $wma) / $row['jumlah_penjualan']);
                 $selisih = $row["jumlah_penjualan"] - $wma;
             } 
+            $total_mape += $mape;
+            $total_wma += $wma;
+            $total_selisih += $selisih;
         ?>
         <tr>
             <td><?= $row["nama_obat"] ?></td>  
             <td><?= format_periode($row["periode"]) ?></td> 
             <td align="center"><?= $row["jumlah_penjualan"] ?></td>
             <td align="center"><?= $index == 0 || $index == 1 ? '-' : $wma ?></td>
+            <td align="center"><?= $index == 0 || $index == 1 ? '-' :  $row["jumlah_penjualan"] - $wma ?></td>
+            <td align="center">
+                <?= $index == 0 || $index == 1 ? '-' : number_format($mape, 5) ?>
+            </td>
         </tr>
     <?php endforeach; ?>
+
+        <tr>
+            <td align="center" colspan="3">Average</td>
+            <td align="center">
+                <?= number_format($total_wma / $total_data, 3) ?>
+            </td>
+            <td align="center">
+                <?= number_format($total_selisih / $total_data, 3) ?>
+            </td>
+            <td align="center">
+                <?= number_format($total_mape / $total_data, 5) ?>
+            </td>
+        </tr>
     </table>
 
-    <div style="margin-top: 24px; margin-bottom: 24px">
+    <div style="margin-top: 24px">
         <?php
         $last_index = count($list) - 1;
         $wma_bulan_selanjutnya = (($list[$last_index]['jumlah_penjualan'] * 2) + ($list[$last_index-1]['jumlah_penjualan'] * 1)) / 3;
+        $mape_bulan_selanjutnya = (abs($list[$last_index]['jumlah_penjualan'] - $wma_bulan_selanjutnya) / $list[$last_index]['jumlah_penjualan']);
         ?>
-        Peramalan untuk periode selanjutnya adalah <?= $wma_bulan_selanjutnya ?>
-    </div>
-
-    <div style="text-align: right">
-        <a href="mape.php?nama_obat=<?= $nama_obat ?>">Klik akurasi error</a>
+        Peramalan untuk periode selanjutnya adalah <?= $wma_bulan_selanjutnya ?> dan mape sebesar <?= number_format($mape_bulan_selanjutnya, 5) ?>%
     </div>
 <?php else: ?>
     0 results
